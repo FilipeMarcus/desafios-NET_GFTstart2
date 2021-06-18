ba056b3580d7ecd7b07070a2939dda98e7e0075a
FilipeMarcus <filipe.amorim@etec.sp.gov.br>
2021-06-18 11:33:26 -0300

    Adiciona desafios 1 e 2

---
+++ InicioCSharp02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Declare Variables
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        
+        //Insira as variaveis de acordo com o solicitado
+        int soma = a + b;
+
+        //Print
+        Console.WriteLine("SOMA = {0}", soma);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# DIO Bootcamp GFT START #2 .NET
+
+### Iniciando a programar em C#
+###### InicioCSharp
+- [ X ] Desafio 01 - Dividindo X por Y (repetido)
+- [ X ] Desafio 02 - Soma Simples
+- [  ] Desafio 03 - Média 1
